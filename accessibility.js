deebf5d724256f05ef32b1ad27407fc5750ebba6
Brent Mercado <brent.mercado.566@my.csun.edu>
2019-04-18 15:57:33 -0700

    Refactor focusMobileNav and focusOutMobileNav

--- accessibility.js
+++ accessibility.js
@@ -1,11 +1,10 @@
+var body = document.querySelector('body');
 var nav = document.querySelector('nav');
 var mobileMenuBtn = document.querySelector('.mobile-menu-btn');
 var mobileMenuList = document.querySelector('.mobile-menu__list');
 var previousFocus;
-var activeMobileNav = false;
 
 mobileMenuBtn.addEventListener('click', toggleMobileNav);
-
 
 // tab trap global vars
 var tabElements;
@@ -16,19 +15,23 @@ nav.addEventListener('focusin', focusMobileNav);
 nav.addEventListener('focusout', focusOutMobileNav);
 
 function focusMobileNav(event) {
-    if (event.target === mobileMenuBtn && mobileMenuList.style.display !== 'block') {
-        mobileMenuBtn.style.backgroundImage = "url(./menu_icon_focus.png)";
-    } else if (event.target === mobileMenuBtn && mobileMenuList.style.display === 'block') {
-        mobileMenuBtn.style.backgroundImage = "url(./close_icon_focus.png)";
+    if (event.target === mobileMenuBtn) {
+        if (mobileMenuList.style.display !== 'block') {
+            mobileMenuBtn.style.backgroundImage = "url(./menu_icon_focus.png)";
+        } else {
+            mobileMenuBtn.style.backgroundImage = "url(./close_icon_focus.png)";
+        }   
     }
 }
 
 function focusOutMobileNav(event) {
-    if (event.target === mobileMenuBtn && mobileMenuList.style.display !== 'block') {
-        mobileMenuBtn.style.backgroundImage = "url(./menu_icon.png)";
-    } else if (event.target === mobileMenuBtn && mobileMenuList.style.display === 'block') {
-        mobileMenuBtn.style.backgroundImage = "url(./close_icon.png)";
-    }
+    if (event.target === mobileMenuBtn) {
+        if (mobileMenuList.style.display !== 'block') {
+            mobileMenuBtn.style.backgroundImage = "url(./menu_icon.png)";
+        } else {
+            mobileMenuBtn.style.backgroundImage = "url(./close_icon.png)";
+        }
+    } 
 }
 
 function toggleMobileNav() {
