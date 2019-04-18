75ad9248c3bb42987ce95fe1271b5696960bbefe
Brent Mercado <brent.mercado.566@my.csun.edu>
2019-04-18 15:28:57 -0700

    Add mobile menu toggle
    Menu button now toggles and displays the correct image while tabbing
    through the mobile navigation.

--- accessibility.js
+++ accessibility.js
@@ -4,7 +4,7 @@ var mobileMenuList = document.querySelector('.mobile-menu__list');
 var previousFocus;
 var activeMobileNav = false;
 
-// mobileMenuBtn.addEventListener('click', openMobileNav);
+mobileMenuBtn.addEventListener('click', toggleMobileNav);
 
 
 // tab trap global vars
@@ -16,27 +16,36 @@ nav.addEventListener('focusin', focusMobileNav);
 nav.addEventListener('focusout', focusOutMobileNav);
 
 function focusMobileNav(event) {
-    if (event.target === mobileMenuBtn) {
+    if (event.target === mobileMenuBtn && mobileMenuList.style.display !== 'block') {
         mobileMenuBtn.style.backgroundImage = "url(./menu_icon_focus.png)";
+    } else if (event.target === mobileMenuBtn && mobileMenuList.style.display === 'block') {
+        mobileMenuBtn.style.backgroundImage = "url(./close_icon_focus.png)";
     }
 }
 
 function focusOutMobileNav(event) {
-    if (event.target === mobileMenuBtn) {
+    if (event.target === mobileMenuBtn && mobileMenuList.style.display !== 'block') {
         mobileMenuBtn.style.backgroundImage = "url(./menu_icon.png)";
+    } else if (event.target === mobileMenuBtn && mobileMenuList.style.display === 'block') {
+        mobileMenuBtn.style.backgroundImage = "url(./close_icon.png)";
     }
 }
 
 function toggleMobileNav() {
     // if mobile nav is open
+    if (mobileMenuList.style.display === 'block') {
         // close the nav menu
+        mobileMenuList.style.display = 'none';
         // set the focus back to menu button
         // change img back to focus-img
-
-    // otherwise
+        mobileMenuBtn.style.backgroundImage = "url(./menu_icon_focus.png)";
+    } else {
         // open the nav menu
+        mobileMenuList.style.display = 'block';
+        mobileMenuBtn.style.backgroundImage = "url(./close_icon_focus.png)";
         // gather tabbable items
         // listen to tab keys pressed 
+    }
 }
 
 
