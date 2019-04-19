7a38e03d179e126221f5b44d3717681d07f062e2
Brent Mercado <brent.mercado.566@my.csun.edu>
2019-04-18 21:41:07 -0700

    Refactor mobile nav tab trapper

--- accessibility.js
+++ accessibility.js
@@ -9,6 +9,9 @@ mobileMenuBtn.addEventListener('click', toggleMobileNav);
 nav.addEventListener('focusin', focusMobileNav);
 nav.addEventListener('focusout', focusOutMobileNav);
 
+// Global Tab Elements
+var tabElements;
+
 // BUG ON USING MOUSE WILL CAUSE FOCUS TO LEAVE MENU BUTTON
 // AND IT WILL CHANGE WHEN IT SHOULDN'T
 
@@ -33,49 +36,55 @@ function focusOutMobileNav(event) {
 }
 
 function toggleMobileNav() {
-    // if mobile nav is open
     if (mobileMenuList.style.display === 'block') {
-        // close the nav menu
-        mobileMenuList.style.display = 'none';
-        // set the focus back to menu button
-        // change img back to focus-img
-        mobileMenuBtn.style.backgroundImage = "url(./menu_icon_focus.png)";
+        closeMobileNav();
     } else {
-        // open the nav menu
-        mobileMenuList.style.display = 'block';
-        mobileMenuBtn.style.backgroundImage = "url(./close_icon_focus.png)";
+        openMobileNav();
         
-        // TAB TRAPPER
-        // gather tabbable items
-        var tabElements = Array.from(document.querySelectorAll('.mobile-menu-btn, .mobile-menu__item a'));
-        var firstTabElement = tabElements[0];
-        var lastTabElement = tabElements[tabElements.length - 1];
-        // listen to tab keys pressed 
-        nav.addEventListener('keydown', mobileTabTrap);
-
-        function mobileTabTrap(event) {
-            if (event.key === 'Tab') {
-                // Shift held down
-                if (event.shiftKey) {
-                    // Backward Tab
-                    if (document.activeElement === firstTabElement) {
-                        event.preventDefault();
-                        lastTabElement.focus();
-                    }
-                } else {
-                    // Forward Tab
-                    if (document.activeElement === lastTabElement) {
-                        event.preventDefault();
-                        firstTabElement.focus();
-                    }
-                }
-            }
-        }
-
-        // event.shiftKey
-        // event.key
+        mobileTabTrap();
     }
 }
 
+function closeMobileNav() {
+    mobileMenuList.style.display = 'none';
+    mobileMenuBtn.style.backgroundImage = "url(./menu_icon_focus.png)";
 
+    nav.removeEventListener('keydown', tabTrap);
+    mobileMenuBtn.focus()
+}
 
+function openMobileNav() {
+    mobileMenuList.style.display = 'block';
+    mobileMenuBtn.style.backgroundImage = "url(./close_icon_focus.png)";
+}
+
+function mobileTabTrap() {
+    // Set global global tabbable variable
+    tabElements = Array.from(document.querySelectorAll('.mobile-menu-btn, .mobile-menu__item a'));
+
+    // listen to tab keys pressed 
+    nav.addEventListener('keydown', tabTrap);
+}
+
+function tabTrap(event) {
+    var firstTabElement = tabElements[0];
+    var lastTabElement = tabElements[tabElements.length - 1];
+    if (event.key === 'Tab') {
+        // Shift held down
+        if (event.shiftKey) {
+            // Backward Tab
+            if (document.activeElement === firstTabElement) {
+                event.preventDefault();
+                lastTabElement.focus();
+            }
+        } else {
+            // Forward Tab
+            if (document.activeElement === lastTabElement) {
+                event.preventDefault();
+                firstTabElement.focus();
+            }
+        }
+    } else if (event.key === 'Escape') {
+        closeMobileNav();
+    }
+}
